4d6b7ed38d1551208516176eaa7931b52f7fb203
ancab <anca_denisa.barbu@cti.pub.ro>
2016-06-27 14:23:33 +0300

    first commit

---
+++ gen/org/flowerplatform/speechrecognizer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.flowerplatform.speechrecognizer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
